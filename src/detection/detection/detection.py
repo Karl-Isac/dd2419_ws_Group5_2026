e3022bbc75b7cde8442d6ac63844461c87a031eb
mr-datian <1158013160@qq.com>
2026-02-11 15:09:46 +0100

    t

--- src/detection/detection/detection.py
+++ src/detection/detection/detection.py
@@ -80,12 +80,32 @@ class Detection(Node):
         colors = np.empty(points.shape, dtype=np.uint32)
 
         red_points = []
-        sum_x = 0
-        sum_y = 0
-        sum_z = 0
-        counter = 0
-
+        red_sum_x = 0
+        red_sum_y = 0
+        red_sum_z = 0
+        red_counter = 0
         tf_red = TransformStamped()
+
+        blue_points = []
+        blue_sum_x = 0
+        blue_sum_y = 0
+        blue_sum_z = 0
+        blue_counter = 0
+        tf_blue = TransformStamped()
+
+        green_points = []
+        green_sum_x = 0
+        green_sum_y = 0
+        green_sum_z = 0
+        green_counter = 0
+        tf_green = TransformStamped()
+
+        wood_points = []
+        wood_sum_x = 0
+        wood_sum_y = 0
+        wood_sum_z = 0
+        wood_counter = 0
+        tf_wood = TransformStamped()
 
         for idx, x in enumerate(gen):
             c = x[3]
@@ -97,30 +117,47 @@ class Detection(Node):
             colors[idx, 2] = np.asarray(pack & 255, dtype=np.uint8)
 
         colors = colors.astype(np.float32) / 255
-        HSV_colors = self.color_converter(colors)
 
         for idx in range(points.shape[0]):
             x, y, z = points[idx]
-            if x < 0.5 and z < 0:
-                if HSV_colors[idx, 0] > 200 and HSV_colors[idx, 0] < 260 and HSV_colors[idx, 1] > 40 and HSV_colors[idx, 2] > 20:
-                    counter += 1
+            if x < 0:
+                # red
+                if colors[idx, 0] > 0.5 and colors[idx, 1] < 0.4 and colors[idx, 2] < 0.4:
+                    red_counter += 1
                     red_points.append([x,y,z])
-                    sum_x += x
-                    sum_y += y
-                    sum_z += z
+                    red_sum_x += x
+                    red_sum_y += y
+                    red_sum_z += z
+                # blue
+                if colors[idx, 0] < 0.4 and colors[idx, 1] < 0.4 and colors[idx, 2] > 0.5:
+                    blue_counter += 1
+                    blue_points.append([x,y,z])
+                    blue_sum_x += x
+                    blue_sum_y += y
+                    blue_sum_z += z
+                # green
+                if colors[idx, 0] < 0.4 and colors[idx, 1] > 0.5 and colors[idx, 2] < 0.4:
+                    green_counter += 1
+                    green_points.append([x,y,z])
+                    green_sum_x += x
+                    green_sum_y += y
+                    green_sum_z += z
+                # wood
+                if colors[idx, 0] > 0.5 and colors[idx, 1] > 0.5 and colors[idx, 2] < 0.4:
+                    wood_counter += 1
+                    wood_points.append([x,y,z])
+                    wood_sum_x += x
+                    wood_sum_y += y
+                    wood_sum_z += z
 
-
-                # if colors[idx, 0] < 0.5 and colors[idx, 1] > 0.5 and colors[idx, 2] < 0.5:
-                    # self.get_logger().info('Green object detected.')       
-           
-        
-        if counter > 10 and not self.red_available:
-            self.get_logger().info('HSV_Blue object detected.')   
+        # red 
+        if red_counter > 10 and not self.red_available:
+            self.get_logger().info('Red object detected.')   
             self.red = tf2_geometry_msgs.PoseStamped()
             self.red.header = msg.header
-            self.red.pose.position.x = sum_x / counter
-            self.red.pose.position.y = sum_y / counter
-            self.red.pose.position.z = sum_z / counter
+            self.red.pose.position.x = red_sum_x / red_counter
+            self.red.pose.position.y = red_sum_y / red_counter
+            self.red.pose.position.z = red_sum_z / red_counter
             self.red.pose.orientation.x = 0.0
             self.red.pose.orientation.y = 0.0
             self.red.pose.orientation.z = 0.0
@@ -169,6 +206,174 @@ class Detection(Node):
             self.static_broadcaster.sendTransform(tf_red)
             self.red_published = True
 
+        # blue
+        if blue_counter > 10 and not self.blue_available:
+            self.get_logger().info('Blue object detected.')   
+            self.blue = tf2_geometry_msgs.PoseStamped()
+            self.blue.header = msg.header
+            self.blue.pose.position.x = blue_sum_x / blue_counter
+            self.blue.pose.position.y = blue_sum_y / blue_counter
+            self.blue.pose.position.z = blue_sum_z / blue_counter
+            self.blue.pose.orientation.x = 0.0
+            self.blue.pose.orientation.y = 0.0
+            self.blue.pose.orientation.z = 0.0
+            self.blue.pose.orientation.w = 1.0
+
+            self.blue_timestamp = msg.header.stamp
+            self.blue_available = True
+
+        if self.blue_available and not self.blue_published:
+            msg_time = rclpy.time.Time.from_msg(self.blue_timestamp)
+            if not self.tf_buffer.can_transform(
+                'realsense_camera_link',
+                self.blue.header.frame_id,
+                msg_time,
+                timeout=rclpy.duration.Duration(seconds=1)
+            ):
+                return
+
+            try:
+                blue_map = self.tf_buffer.transform(
+                    self.blue,
+                    'realsense_camera_link',
+                    timeout=rclpy.duration.Duration(seconds=1)
+                )
+            except TransformException as ex:
+                self.get_logger().info(
+                    f'Could not transform blue object from '
+                    f'{self.blue.header.frame_id} to map: {ex}'
+                )
+                return
+            
+            tf_blue.header.stamp = self.blue_timestamp
+
+            tf_blue.header.frame_id = 'realsense_camera_link'
+            tf_blue.child_frame_id = 'blue_object'
+
+            tf_blue.transform.translation.x = blue_map.pose.position.x
+            tf_blue.transform.translation.y = blue_map.pose.position.y
+            tf_blue.transform.translation.z = blue_map.pose.position.z
+
+            tf_blue.transform.rotation.x = 0.0
+            tf_blue.transform.rotation.y = 0.0
+            tf_blue.transform.rotation.z = 0.0
+            tf_blue.transform.rotation.w = 1.0
+
+            self.static_broadcaster.sendTransform(tf_blue)
+            self.blue_published = True
+        
+        # green
+        if green_counter > 10 and not self.green_available:
+            self.get_logger().info('Green object detected.')   
+            self.green = tf2_geometry_msgs.PoseStamped()
+            self.green.header = msg.header
+            self.green.pose.position.x = green_sum_x / green_counter
+            self.green.pose.position.y = green_sum_y / green_counter
+            self.green.pose.position.z = green_sum_z / green_counter
+            self.green.pose.orientation.x = 0.0
+            self.green.pose.orientation.y = 0.0
+            self.green.pose.orientation.z = 0.0
+            self.green.pose.orientation.w = 1.0
+
+            self.green_timestamp = msg.header.stamp
+            self.green_available = True
+        
+        if self.green_available and not self.green_published:
+            msg_time = rclpy.time.Time.from_msg(self.green_timestamp)
+            if not self.tf_buffer.can_transform(
+                'realsense_camera_link',
+                self.green.header.frame_id,
+                msg_time,
+                timeout=rclpy.duration.Duration(seconds=1)
+            ):
+                return
+
+            try:
+                green_map = self.tf_buffer.transform(
+                    self.green,
+                    'realsense_camera_link',
+                    timeout=rclpy.duration.Duration(seconds=1)
+                )
+            except TransformException as ex:
+                self.get_logger().info(
+                    f'Could not transform green object from '
+                    f'{self.green.header.frame_id} to map: {ex}'
+                )
+                return
+            
+            tf_green.header.stamp = self.green_timestamp
+
+            tf_green.header.frame_id = 'realsense_camera_link'
+            tf_green.child_frame_id = 'green_object'
+
+            tf_green.transform.translation.x = green_map.pose.position.x
+            tf_green.transform.translation.y = green_map.pose.position.y
+            tf_green.transform.translation.z = green_map.pose.position.z
+
+            tf_green.transform.rotation.x = 0.0
+            tf_green.transform.rotation.y = 0.0
+            tf_green.transform.rotation.z = 0.0
+            tf_green.transform.rotation.w = 1.0
+
+            self.static_broadcaster.sendTransform(tf_green)
+            self.green_published = True
+
+        # wood
+        if wood_counter > 10 and not self.wood_available:
+            self.get_logger().info('Wood object detected.')   
+            self.wood = tf2_geometry_msgs.PoseStamped()
+            self.wood.header = msg.header
+            self.wood.pose.position.x = wood_sum_x / wood_counter
+            self.wood.pose.position.y = wood_sum_y / wood_counter
+            self.wood.pose.position.z = wood_sum_z / wood_counter
+            self.wood.pose.orientation.x = 0.0
+            self.wood.pose.orientation.y = 0.0
+            self.wood.pose.orientation.z = 0.0
+            self.wood.pose.orientation.w = 1.0
+
+            self.wood_timestamp = msg.header.stamp
+            self.wood_available = True
+        
+        if self.wood_available and not self.wood_published:
+            msg_time = rclpy.time.Time.from_msg(self.wood_timestamp)
+            if not self.tf_buffer.can_transform(
+                'realsense_camera_link',
+                self.wood.header.frame_id,
+                msg_time,
+                timeout=rclpy.duration.Duration(seconds=1)
+            ):
+                return
+
+            try:
+                wood_map = self.tf_buffer.transform(
+                    self.wood,
+                    'realsense_camera_link',
+                    timeout=rclpy.duration.Duration(seconds=1)
+                )
+            except TransformException as ex:
+                self.get_logger().info(
+                    f'Could not transform wood object from '
+                    f'{self.wood.header.frame_id} to map: {ex}'
+                )
+                return
+            
+            tf_wood.header.stamp = self.wood_timestamp
+
+            tf_wood.header.frame_id = 'realsense_camera_link'
+            tf_wood.child_frame_id = 'wood_object'
+
+            tf_wood.transform.translation.x = wood_map.pose.position.x
+            tf_wood.transform.translation.y = wood_map.pose.position.y
+            tf_wood.transform.translation.z = wood_map.pose.position.z
+
+            tf_wood.transform.rotation.x = 0.0
+            tf_wood.transform.rotation.y = 0.0
+            tf_wood.transform.rotation.z = 0.0
+            tf_wood.transform.rotation.w = 1.0
+
+            self.static_broadcaster.sendTransform(tf_wood)
+            self.wood_published = True
+
         # if self.red_available and not self.red_published:
         #     msg_time = rclpy.time.Time.from_msg(self.red_timestamp)
         #     if not self.tf_buffer.can_transform(
@@ -209,31 +414,7 @@ class Detection(Node):
         #     self.static_broadcaster.sendTransform(tf_red)
         #     self.red_published = True
         
-    def color_converter(self, RGB_colors):
-        RGB_colors = RGB_colors.astype(np.float32) / 255.0
-        HSV_colors = np.empty(RGB_colors.shape, dtype=np.float32)
-        for idx in range(RGB_colors.shape[0]):
-            r, g, b = RGB_colors[idx]
-            mx = max(r, g, b)
-            mn = min(r, g, b)
-            h = 0
-            s = 0
-            v = mx
-
-            if mx != 0:
-                s = (mx - mn) / mx
-                if r == mx:
-                    h = (g - b) / (mx - mn)
-                elif g == mx:
-                    h = 2 + (b - r) / (mx - mn)
-                else:
-                    h = 4 + (r - g) / (mx - mn)
-            
-            if h < 0:
-                h += 6
-
-            HSV_colors[idx] = np.array([h * 60, s * 100, v * 100], dtype=np.float32)
-        return HSV_colors
+        
  
 def main():
     rclpy.init()
