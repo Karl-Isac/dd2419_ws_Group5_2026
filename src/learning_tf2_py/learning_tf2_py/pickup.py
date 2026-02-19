9ddf378845962f1013225a6c7ff5acd2e2f79193
Grumpy Robot <ericson8@kth.se>
2026-02-19 21:18:28 +0100

    arm control bugfixes, PI control sideways, still have to tune

--- src/learning_tf2_py/learning_tf2_py/pickup.py
+++ src/learning_tf2_py/learning_tf2_py/pickup.py
@@ -52,6 +52,20 @@ def is_square(approx):
 
         if cos_angle > 0.3:  # ~72–108 degrees                      # might need to finetune
             return False
+        
+    # Side length check (Is it a square or a rectangle)
+    side_lengths = []
+    for i in range(4):
+        p1 = pts[i]
+        p2 = pts[(i + 1) % 4]
+        side_length = np.linalg.norm(p1 - p2)
+        side_lengths.append(side_length)
+    min_side = min(side_lengths)
+    max_side = max(side_lengths)
+
+    aspect_tolerance = 0.2   # 20% tolerance, tunable parameter
+    if (max_side - min_side) / max_side > aspect_tolerance:
+        return False
 
     return True
 
@@ -66,11 +80,15 @@ def draw_cs_on_image(image,centerpoint,angle):
     cv2.arrowedLine(image, (int(cx), int(cy)), (int(cx-arrow_length*math.sin(theta)),int(cy+arrow_length*math.cos(theta))),(0,255,0),2)
 
 def saturate_difference(current,previous,limit):
+    print(previous + limit)
+    print(previous - limit)
     if abs(current - previous) > limit:
         if (current - previous) > 0:
             return previous + limit
         else:
             return previous - limit
+    else:
+        return current
 
         
 
@@ -93,18 +111,13 @@ class Pickup(Node):
             Image, '/arm/camera/image_raw', self.image_callback, 10)
         
         # Initialize the arm position
+        self.init_position = [10,120,30,180,180,120]            # debug so it cannot collide [10,120,30,180,100,120]
         msg = ArmControl()
         msg.header.stamp = self.get_clock().now().to_msg()
         msg.time = [1000,1000,1000,1000,1000,1000]
-        msg.position = [10,120,30,180,100,120]
+        msg.position = self.init_position
         self._pub_control.publish(msg)
         time.sleep(1)
-
-
-        
-        # Send out a control action every 0.1 seconds
-        timer_period = 0.1  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
 
         
         self.cube_position_available = False
@@ -112,44 +125,70 @@ class Pickup(Node):
         self.joint5target = 120
         self.joint1target = 120
 
+        self.sideways_integral_term = 0
+
+        # Send out a control action every 0.1 seconds
+        timer_period = 0.1   #debug 0.1  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
 
     def timer_callback(self):
         if self.cube_position_available:
-            # Control gains:
-            k_sideways = 0.2
-            k_rotation = 1
+            try:
+                start = time.perf_counter()
+                # Control gains:
+                k_sideways = 0.01#0.05                  TODO tune a bit more, commented values work with 0.5 sec timer, maybe add D
+                k_sideways_integral = 0.02#0.1
+                k_rotation = 1
 
-            # Previous targets:
-            prev_joint1target = self.joint1target
-            prev_joint5target = self.joint5target
+                # Previous targets:
+                prev_joint1target = self.joint1target
+                prev_joint5target = self.joint5target
 
-            image_half_width = self.image_shape[1]/2
-            cx,cy = self.cube_position_in_frame
-            rotation = self.cube_orientation_in_frame
-            self.cube_position_available = False
-            # Sideways control
-            sideways_error = image_half_width-cx
-            self.joint5target = 120 + k_sideways*sideways_error
-            # Rotation control
-            rotation_error = rotation % 90
-            if rotation_error > 45:
-                rotation_error = rotation_error - 90
-            self.joint1target = 120 + k_rotation*rotation_error
+                image_half_width = self.image_shape[1]/2
+                cx,cy = self.cube_position_in_frame
+                rotation = self.cube_orientation_in_frame
+                self.cube_position_available = False
+                # Sideways control (PI)
+                sideways_error = image_half_width-cx
+                self.sideways_integral_term = self.sideways_integral_term + k_sideways_integral*sideways_error
+                self.joint5target = 120 + k_sideways*sideways_error + self.sideways_integral_term
+                
+                # Rotation control (P)
+                rotation_error = rotation % 90
+                if rotation_error > 45:
+                    rotation_error = rotation_error - 90
+                self.joint1target = 120 + k_rotation*rotation_error
 
-            # Max motor speed 60 deg / 0.22 sec
-            # => 25 deg per 0.1 tick
-            limit = 25
-            self.joint1target = saturate_difference(self.joint1target,prev_joint1target,limit)
-            self.joint5target = saturate_difference(self.joint5target,prev_joint5target,limit)
-            
+                # Max motor speed 60 deg / 0.22 sec
+                # => 25 deg per 0.1 tick
+                limit = 25
 
-            msg = ArmControl()
-            msg.header.stamp = self.get_clock().now().to_msg()
-            msg.time = [1000,1000,1000,1000,1000,1000]
-            msg.position = [10,self.joint1target,30,180,100,self.joint5target]
-            print("joint1target: " + str(self.joint1target))
-            print("joint5target: " + str(self.joint5target))
-            self._pub_control.publish(msg)
+                # Check whether all of these are reals, had some issues previously:
+                assert all(isinstance(v, (int, float)) for v in (self.joint1target,prev_joint1target,self.joint5target,prev_joint5target,limit))
+
+                self.joint1target = saturate_difference(self.joint1target,prev_joint1target,limit)
+                self.joint5target = saturate_difference(self.joint5target,prev_joint5target,limit)
+                self.get_logger().info("Previous target: " + str(prev_joint5target))
+                self.get_logger().info("Current target: " + str(self.joint5target)+"\n")
+
+                msg = ArmControl()
+                msg.header.stamp = self.get_clock().now().to_msg()
+                msg.time = [100]*6  # move all joints in 100 ms (timer period), safe bcuz of saturation just above
+                msg.position = [self.init_position[0],self.joint1target,self.init_position[2],self.init_position[3],self.init_position[4],self.joint5target]
+
+                self._pub_control.publish(msg)
+                end = time.perf_counter()
+                print(f"Execution time: {end - start:.6f} seconds")
+            except Exception as e:
+                # If something breaks in the controller, goto initial position, assumed to be safe:
+                self.get_logger().fatal("Error in controller code, moving to safe state and shutting down")
+                msg = ArmControl()
+                msg.header.stamp = self.get_clock().now().to_msg()
+                msg.time = [3000,3000,3000,3000,3000,3000]
+                msg.position = self.init_position
+                self._pub_control.publish(msg)
+                raise
         return
         
         
@@ -217,7 +256,6 @@ class Pickup(Node):
         
 
 def main():
-    raise("dont run it, talk to Andrew first")
     rclpy.init()
     node = Pickup()
     try:
