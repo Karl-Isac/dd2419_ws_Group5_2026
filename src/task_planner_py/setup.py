f64f1bf36126433f7e29fa17213e40786ba94456
Magnus Ericson <magnus99ericson@gmail.com>
2026-03-01 18:44:56 +0100

    added new launch file for test

--- src/task_planner_py/setup.py
+++ src/task_planner_py/setup.py
@@ -29,6 +29,7 @@ setup(
         'console_scripts': [
             'task_planner_node = task_planner_py.task_planner_node:main',
             'task_planner_node_2 = task_planner_py.task_planner_node_2:main',
+            'task_planner_node_3 = task_planner_py.task_planner_node_3:main',
             'fake_arm_node = task_planner_py.fake_arm_node:main',
         ],
     },
