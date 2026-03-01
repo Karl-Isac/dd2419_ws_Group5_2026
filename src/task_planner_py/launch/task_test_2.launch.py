9cd16988bf57794e9570a5cb7b942f84e95f20be
Magnus Ericson <magnus99ericson@gmail.com>
2026-03-01 20:36:21 +0100

    update launch file

--- src/task_planner_py/launch/task_test_2.launch.py
+++ src/task_planner_py/launch/task_test_2.launch.py
@@ -73,6 +73,26 @@ def generate_launch_description():
         output='screen'
     )
 
+    fake_objects = ExecuteProcess(
+        cmd=[
+            'ros2', 'topic', 'pub', '/detected_objects',
+            'geometry_msgs/msg/PoseArray',
+            '{header: {frame_id: map}, poses: [{position: {x: 1.0, y: 0.5, z: 0.0}, orientation: {w: 1.0}}]}',
+            '-r', '1'
+        ],
+        output='screen'
+    )
+
+    fake_boxes = ExecuteProcess(
+        cmd=[
+            'ros2', 'topic', 'pub', '/detected_boxes',
+            'geometry_msgs/msg/PoseArray',
+            '{header: {frame_id: map}, poses: [{position: {x: 2.0, y: 1.5, z: 0.0}, orientation: {w: 1.0}}]}',
+            '-r', '1'
+        ],
+        output='screen'
+    )
+
     return LaunchDescription([
         controller,
         planner,
