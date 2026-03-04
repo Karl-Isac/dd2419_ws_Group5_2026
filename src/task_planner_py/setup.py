e9a54cbe74e15b22b4650d71a681b7b813b71209
Magnus Ericson <magnus99ericson@gmail.com>
2026-03-04 17:39:32 +0100

    Created new launch file for arm, detection, navigation and task_planner

--- src/task_planner_py/setup.py
+++ src/task_planner_py/setup.py
@@ -31,6 +31,7 @@ setup(
             'task_planner_node_2 = task_planner_py.task_planner_node_2:main',
             'task_planner_node_3 = task_planner_py.task_planner_node_3:main',
             'task_planner_node_posearray = task_planner_py.task_planner_node_posearray:main',
+            'task_planner_node_posearray_2 = task_planner_py.task_planner_node_posearray_2:main',
             'fake_arm_node = task_planner_py.fake_arm_node:main',
         ],
     },
