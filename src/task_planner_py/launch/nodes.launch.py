3d6e4b7cf603873b280f4018b88300b4df4c11c7
Magnus Ericson <magnus99ericson@gmail.com>
2026-04-10 17:44:37 +0200

    updated launch files

--- src/task_planner_py/launch/nodes.launch.py
+++ src/task_planner_py/launch/nodes.launch.py
@@ -1,48 +1,79 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import AnyLaunchDescriptionSource
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
-import os
 
 def generate_launch_description():
 
-    return LaunchDescription([
+    arm_safe_republisher = Node(
+        package='learning_tf2_py',
+        executable='arm_safe_republisher',
+        name='arm_safe_republisher',
+        output='screen'
+    )
 
-        Node(
-            package='learning_tf2_py',
-            executable='arm_safe_republisher',
-            name='arm_safe_republisher',
-            output='screen'
-        ),
-        Node(
-            package='learning_tf2_py',
-            executable='arm_control',
-            name='arm_control',
-            output='screen'
-        ),
-        Node(
-            package='task_planner_py',
-            executable='task_planner',
-            name='task_planner',
-            output='screen'
-        ),
-        Node(
-            package='odometry',
-            executable='odometry',
-            name='odometry',
-            output='screen'
-        ),
-        Node(
-            package='mapping',
-            executable='fix_pose',
-            name='fix_pose',
-            output='screen'
-        ),
-        Node(
-            package='grumpy_navigation_py',
-            executable='fake_obstacles',
-            name='fake_obstacles',
-            output='screen'
-        ),
+    arm_control = Node(
+        package='learning_tf2_py',
+        executable='arm_control',
+        name='arm_control',
+        output='screen'
+    )
+
+    task_planner = Node(
+        package='task_planner_py',
+        executable='task_planner',
+        name='task_planner',
+        output='screen'
+    )
+
+    odometry = Node(
+        package='odometry',
+        executable='odometry',
+        name='odometry',
+        output='screen'
+    )
+
+    fix_pose = Node(
+        package='mapping',
+        executable='fix_pose',
+        name='fix_pose',
+        output='screen'
+    )
+
+    fake_obstacles = Node(
+        package='grumpy_navigation_py',
+        executable='fake_obstacles',
+        name='fake_obstacles',
+        output='screen'
+    )
+
+    planner = Node(
+        package='grumpy_navigation_py',
+        executable='planner',
+        name='planner',
+        output='screen'
+    )
+
+    controller = Node(
+        package='grumpy_navigation_py',
+        executable='controller',
+        name='controller',
+        output='screen'
+    )
+
+    map_to_odom = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='map_to_odom',
+        arguments=['0.49', '0.5', '0', '0', '0', '0', 'map', 'odom']
+    )
+
+    return LaunchDescription([
+        arm_safe_republisher,
+        arm_control,
+        task_planner,
+        odometry,
+        fix_pose,
+        fake_obstacles,
+        planner,
+        controller,
+        map_to_odom,
     ])
